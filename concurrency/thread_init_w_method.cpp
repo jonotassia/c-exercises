daaff269e6445f741d8ddf557ec184199164f1a4
Jono Tassia <jonotassia@gmail.com>
2023-03-09 20:55:57 +1100

    Concurrency lesson start

---
+++ concurrency/thread_init_w_method.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <memory>
+
+class Vehicle
+{
+public:
+    Vehicle() : _id(0) {}
+    void addID(int id) { _id = id; }
+    void printID()
+    {
+        std::cout << "Vehicle ID=" << _id << std::endl;
+    }
+    // TODO: Modify the Vehicle class as indicated in the instructions on the left.
+    void setName(std::string str) {
+        _name = str;
+    }
+    std::string getName() {
+        return _name;
+    }
+private:
+    int _id;
+    std::string _name;
+};
+
+int main()
+{
+    // create thread
+    std::shared_ptr<Vehicle> v(new Vehicle);
+    std::thread t = std::thread(&Vehicle::addID, v, 1); // call member function on object v
+    
+    // TODO: Modify the main to start setName as a thread.
+    std::thread t2 = std::thread(&Vehicle::setName, v, "Jono");
+    // Also, add code to main to print the name of the vehicle.
+    
+    // wait for thread to finish
+    t.join();
+    t2.join();
+    
+    // print Vehicle id
+    v->printID();
+    std::cout << v->getName();
+    
+    return 0;
+}
